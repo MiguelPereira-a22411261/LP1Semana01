97cf9db2847b47c29a6f538904644048207815e4
Miguel Pereira <miguel.pereirawork08@gmail.com>
2025-02-20 20:26:34 +0000

    Int type variables added

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+            int i = 1;
+            uint j = 1U;
+            Console.WriteLine(i + " " + j);
+        }
+    }
+}
